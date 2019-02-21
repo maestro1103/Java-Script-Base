a1b952cdc31a8f75aa886e65d3513d14750a055d
Maks Horbatiuk <maksgorbatuk6@gmail.com>
2019-02-21 20:03:04 +0200

    JS Lesson

---
+++ common.js
@@ -0,0 +1,14 @@
+
+console.log("Hello world!");
+
+var age = 20;
+console.log("Age: ", age);
+console.log(typeof age);
+
+var value= 3.14;
+console.log(value);
+console.log(typeof value);
+
+var name="Bill";
+console.log(name);
+console.log(typeof name);
